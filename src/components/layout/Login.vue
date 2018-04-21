18026b6411c41f3a390f62d8108f312ef2523371
denglingbo <denglingbo@126.com>
2018-04-21 18:03:10 +0800

    创建 auth 路由

---
+++ src/components/layout/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><button @click="handleLogin">Login</button></div>
+</template>
+
+<script lang="ts">
+import { Action } from 'vuex-class'
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Layout extends Vue {
+  @Action('auth/login') login: any
+
+  handleLogin() {
+    this.login()
+    this.$router.replace('/')
+  }
+}
+</script>
+<style lang="scss">
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,87 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import routes from './routes'
-// import store from '../store'
+import constantRoutes from './routes'
+import store from '../store'
 // import { ssoLogin, hasTokenRedirect, replaceTokenByUrl } from '../util/auth'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  routes,
+  routes: constantRoutes,
 })
 
+/**
+ * 判断是否是静态路由
+ * @param path route.to.path
+ */
+function isConstantRoute(path) {
+  return constantRoutes.find(item => item.path === path) !== undefined
+}
+
+/**
+ * 创建所有路由
+ * @param action store.actions
+ */
+function creaetRoutesFn(action) {
+  return new Promise(resolve => {
+    // 添加 auth role
+    action.then(() => {
+      const routes = store.getters['auth/routes']
+      router.addRoutes(routes)
+
+      resolve()
+    })
+  })
+}
+
+/**
+ * 路由守卫
+ * @param to router -> beforeEach to
+ * @param next router -> beforeEach next
+ */
+function beforeRole(to, next) {
+  const role = store.getters['auth/role']
+
+  // 如果没有 role 就绪，进行 role 获取
+  if (!role) {
+    const action = store.dispatch('auth/getRoles')
+
+    // 如果在静态路由中 则直接进入
+    if (isConstantRoute(to.path)) {
+      // 添加到所有路由
+      creaetRoutesFn(action)
+      // 进入路由
+      next()
+    } else {
+      const hackto: any = { ...to, replace: true }
+
+      creaetRoutesFn(action).then(() => next(hackto))
+    }
+  } else {
+    next()
+  }
+}
+
+const whiteList = ['/login']
+
 router.beforeEach((to, from, next) => {
-  next()
-  // const hasToken = hasTokenRedirect()
+  const token = store.getters['auth/token']
 
-  // if (hasToken) {
-  //   store.dispatch('auth/SET_TOKEN', hasToken)
-
-  //   // 重定向页面，去除 token
-  //   replaceTokenByUrl()
-  // }
-
-  // const auth = store.getters['auth/token']
-
-  // if (auth) {
-  //   next()
-  // } else {
-  //   ssoLogin()
-  // }
+  if (token) {
+    if (to.path === '/login') {
+      next({ path: '/' })
+    } else {
+      beforeRole(to, next)
+    }
+  } else {
+    if (whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      // to login
+      next('/login')
+    }
+  }
 })
 
 export default router
