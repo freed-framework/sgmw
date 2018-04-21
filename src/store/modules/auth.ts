f71d3e29d77bf2911996464a10984e7c7aecce99
denglingbo <denglingbo@126.com>
2018-04-22 01:09:24 +0800

    创建 menu

--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -1,10 +1,11 @@
 import ls from '@/util/localStorage'
 import { ssoLogin, ssoLogout } from '@/util/auth'
 import { constantRoutes, asyncRoutes } from '@/router/routes'
-import { getRole } from '@/api'
+import { getRole, login } from '@/api'
 
 const TOKEN_KEY = 'TOKEN'
-
+// 获取静态路由
+const constantMenus = getMenus(constantRoutes)
 const ActionType = {
   // 设置 Token
   SET_TOKEN: 'SET_TOKEN',
@@ -14,13 +15,16 @@ const ActionType = {
   LOGIN_ACCESS: 'LOGIN_ACCESS',
   // 设置路由
   SET_ROUTERS: 'SET_ROUTERS',
+  SET_MENUS: 'SET_MENUS',
   // 是否完成权限接口
   ROLE_DONE: 'ROLE_DONE',
+  // 设置用户信息
+  SET_USER: 'SET_USER',
 }
 
 export interface StateType {
-  token: null | string,
-  userInfo: {} | Object,
+  token: string | null,
+  user: {} | Object,
   [prop: string]: any
 }
 
@@ -28,11 +32,13 @@ const state: StateType = {
   // 储存用户 token, 暂时默认为 true
   token: null,
   // 用户信息
-  userInfo: {},
+  user: {},
   // 判断是否拉取了权限
   role: false,
   // 可用路由
-  routes: constantRoutes || []
+  routes: constantRoutes || [],
+  // 左侧导航
+  menus: constantMenus
 }
 
 const mutations = {
@@ -57,8 +63,17 @@ const mutations = {
   [ActionType.SET_ROUTERS](state: StateType, authRoutes: Array<any>) {
     state.routes = constantRoutes.concat(authRoutes)
   },
+  [ActionType.SET_MENUS](state: StateType, authRoutes: Array<any>) {
+    const menus = getMenus(authRoutes)
+    state.menus = constantMenus.concat(menus)
+  },
   [ActionType.ROLE_DONE](state: StateType, done: boolean) {
     state.role = done
+  },
+  [ActionType.SET_USER](state: StateType, payload: any) {
+    state.user = {
+      ...payload
+    }
   }
 }
 
@@ -76,14 +91,19 @@ const actions = {
     ssoLogin()
   },
   // 登录
-  login({ commit }, options) {
-    commit(ActionType.SET_TOKEN, '123')
+  async login({ commit }, params) {
+    const res = await login(params)
+
+    commit(ActionType.SET_USER, res.data)
+    commit(ActionType.SET_TOKEN, '1')
   },
   // 通过权限接口的返回创建最终路由
   async getRoles({ commit }, data) {
     const res = await getRole()
+    const authRoutes = getAuthRoutes(res)
 
-    commit(ActionType.SET_ROUTERS, getAuthRoutes(res))
+    commit(ActionType.SET_MENUS, authRoutes)
+    commit(ActionType.SET_ROUTERS, authRoutes)
     commit(ActionType.ROLE_DONE, true)
   }
 }
@@ -127,10 +147,39 @@ function getAuthRoutes(roles) {
   })
 }
 
+/**
+ * 创建 Menus
+ * @param routes routes
+ */
+function getMenus(routes: any = []) {
+  const menus = []
+
+  routes.forEach((route) => {
+    // 创建 menu 数据格式
+    const menu: any = {
+      path: route.path,
+      ...(route.name && { name: route.name }),
+      ...(route.meta && { text: route.meta.text }),
+      ...(route.hidden && { hidden: route.hidden }),
+    }
+
+    if (!menu.text && route.children && route.children.length === 1) {
+      menu.text = route.children[0].meta ? route.children[0].meta.text : ''
+    } else {
+      menu.children = getMenus(route.children)
+    }
+
+    menus.push(menu)
+  })
+
+  return menus
+}
+
 const getters = {
   token: (state) => state.token || ls.get(TOKEN_KEY),
   role: (state) => state.role,
-  routes: (state) => state.routes
+  routes: (state) => state.routes,
+  menus: (state) => state.menus
 }
 
 export default {
