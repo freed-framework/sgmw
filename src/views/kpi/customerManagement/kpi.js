f6ab038c1ed136a62b5d8bf0ea89429015ca6028
jikequan <jikequan@163.com>
2018-05-05 17:34:12 +0800

    清楚无效数据

--- src/views/kpi/customerManagement/kpi.js
+++ src/views/kpi/customerManagement/kpi.js
@@ -1,23 +1,93 @@
-export const kpi = [
-  {label: '潜客跟进及时率'},
-  {label: '邀约到店'},
-  {label: '新增潜客试乘试驾率'},
-  {label: '潜客试乘试驾率'},
-  {label: '展厅试乘试驾率'},
-  {label: '线索潜客转化'},
-  {label: '潜客成交率'},
-  {label: '新增潜客成交率'},
-  {label: '新增线索邀约转化潜客成交率'},
-  {label: '结转潜客成交率'},
-  {label: '展厅客户成交率'},
-  {label: '潜客战败率'},
-  {label: '新增潜客战败率'},
-  {label: '结转潜客战败率'},
-  {label: '到店客户战败率'},
-  {label: '新增潜客订单合同转化率'},
-  {label: '结转潜客订单合同转化率'},
-  {label: '潜客订单合同转化率'},
-  {label: '新增订单合同交付率'},
-  {label: '结转订单合同交付率'},
-  {label: '订单合同交付率'}
-]
+// export const kpi = [
+//   {label: '潜客跟进及时率'},
+//   {label: '邀约到店'},
+//   {label: '新增潜客试乘试驾率'},
+//   {label: '潜客试乘试驾率'},
+//   {label: '展厅试乘试驾率'},
+//   {label: '线索潜客转化'},
+//   {label: '潜客成交率'},
+//   {label: '新增潜客成交率'},
+//   {label: '新增线索邀约转化潜客成交率'},
+//   {label: '结转潜客成交率'},
+//   {label: '展厅客户成交率'},
+//   {label: '潜客战败率'},
+//   {label: '新增潜客战败率'},
+//   {label: '结转潜客战败率'},
+//   {label: '到店客户战败率'},
+//   {label: '新增潜客订单合同转化率'},
+//   {label: '结转潜客订单合同转化率'},
+//   {label: '潜客订单合同转化率'},
+//   {label: '新增订单合同交付率'},
+//   {label: '结转订单合同交付率'},
+//   {label: '订单合同交付率'}
+// ]
+// cluePotentialCustomerConversionRate 线索潜客转化率
+// newOrderContractDeliveryRate 新增订单合同交付率
+// newPotentialCustomerDefeatRate 新增潜客战败率
+// newPotentialCustomerOrderContractConversionRate 新增潜客订单合同转化率
+// newPotentialCustomerTryDriveRate 新增潜客试乘试驾率
+// newPotentialCustomerTurnoverRate 新增潜客成交率
+// orderContractDeliveryRate 订单合同交付率
+// potentialCustomerDefeatRate 潜客战败率
+// potentialCustomerFollowInTimeRate 潜客跟进及时率
+// potentialCustomerOrderContractConversionRate 潜客订单合同转化率
+// potentialCustomerTryDriveRate 潜客试乘试驾率
+// potentialCustomerTurnoverRate 潜客成交率
+// storeCustomerDefeatRate 到店客户战败率
+// transferOrderContractDeliveryRate 结转订单合同交付率
+// transferPotentialCustomerDefeatRate 结转潜客战败率
+// transferPotentialCustomerOrderContractConversionRate 结转潜客订单合同转化率
+// transferPotentialCustomerTurnoverRate 结转潜客成交率
+// kip类型
+export const kpi = [{
+  label: '线索潜客转化率',
+  value: 'cluePotentialCustomerConversionRate'
+}, {
+  label: '新增订单合同交付率',
+  value: 'newOrderContractDeliveryRate'
+}, {
+  label: '新增潜客战败率',
+  value: 'newPotentialCustomerDefeatRate'
+}, {
+  label: '新增潜客订单合同转化率',
+  value: 'newPotentialCustomerOrderContractConversionRate'
+}, {
+  label: '新增潜客试乘试驾率',
+  value: 'newPotentialCustomerTryDriveRate'
+}, {
+  label: '新增潜客成交率',
+  value: 'newPotentialCustomerTurnoverRate'
+}, {
+  label: '订单合同交付率',
+  value: 'orderContractDeliveryRate'
+}, {
+  label: '潜客战败率',
+  value: 'potentialCustomerDefeatRate'
+}, {
+  label: '潜客跟进及时率',
+  value: 'potentialCustomerFollowInTimeRate'
+}, {
+  label: '潜客订单合同转化率',
+  value: 'potentialCustomerOrderContractConversionRate'
+}, {
+  label: '潜客试乘试驾率',
+  value: 'potentialCustomerTryDriveRate'
+}, {
+  label: '潜客成交率',
+  value: 'potentialCustomerTurnoverRate'
+}, {
+  label: '到店客户战败率',
+  value: 'storeCustomerDefeatRate'
+}, {
+  label: '结转订单合同交付率',
+  value: 'transferOrderContractDeliveryRate'
+}, {
+  label: '结转潜客战败率',
+  value: 'transferPotentialCustomerDefeatRate'
+}, {
+  label: '结转潜客订单合同转化率',
+  value: 'transferPotentialCustomerOrderContractConversionRate'
+}, {
+  label: '结转潜客成交率',
+  value: 'transferPotentialCustomerTurnoverRate'
+}]
