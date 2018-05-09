3d107c2fb5eafdb7e76f6516926672ba48550d3d
jikequan <jikequan@163.com>
2018-05-09 21:35:21 +0800

    merge

--- src/views/distributor/inspectionSystem/index.vue
+++ src/views/distributor/inspectionSystem/index.vue
@@ -62,7 +62,7 @@
                 </el-col>
               </el-row>
               <el-row>
-                <el-col :span="6" :offset="18">
+                <el-col :span="6" :offset="18" style="margin-bottom: 20px;">
                   <el-button type="primary" @click="submitForm('form')">检索</el-button>
                   <el-button type="success" @click="exportList('form')">导出</el-button>
                   <el-button @click="resetForm('form')">重置</el-button>
