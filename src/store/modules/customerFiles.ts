2da4fac8e84939356bb3ac45f694dba60a57c655
jikequan <jikequan@163.com>
2018-06-01 16:38:40 +0800

    修改数据排序

--- src/store/modules/customerFiles.ts
+++ src/store/modules/customerFiles.ts
@@ -1,6 +1,8 @@
 import { customerFiles } from '../../api'
 import { formatData, initList } from '../helpers'
 
+const fields = ['经销商号', '经销商名称', '车架号', '客户姓名', '联系电话', '建档日期', '交车日期', '开票日期', '销量统计日期', '状态', '经销商审核日期', 'SGMW审核结果', '申请修改状态', '集团车', '合格证号', '制造日期', '基地', '车型']
+
 const ActionType = {
   FETCH_CUSTOM_STATIST_LIST: 'FETCH_CUSTOM_STATIST_LIST',
 }
@@ -25,6 +27,7 @@ const actions = {
 const mutations = {
   [ActionType.FETCH_CUSTOM_STATIST_LIST](state: any, payload: any) {
     const { data = [] } = payload
+    data.fields = fields
     const list = formatData(data)
     state.list = list
   },
