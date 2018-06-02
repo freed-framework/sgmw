7e0e179d1714eb06df5dd75e837f9dc32edb90d2
denglingbo <denglingbo@126.com>
2018-06-02 14:47:13 +0800

    .

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -43,6 +43,45 @@
       </div>
     </el-main>
     <!-- <el-footer></el-footer> -->
+
+    <!--  -->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogDelVisible"
+      width="300px"
+      :show-close="false"
+      :close-on-click-modal="false"
+      center
+    >
+      <div style="text-align: center">密码即将过期，请尽快修改密码</div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="handle2Login">继续登陆</el-button>
+        <el-button :loading="dialogDelLoading" type="primary" @click="confirmDel">立即修改</el-button>
+      </span>
+    </el-dialog>
+
+        <!-- 修改密码 -->
+    <el-dialog title="修改密码" width="480px" :visible.sync="dialogPwdVisible" :close-on-click-modal="false">
+      <el-form
+        :model="formPwd"
+        :rules="rulesPwd"
+        ref="formPwd"
+      >
+        <el-form-item prop="old" label="原密码" :label-width="formLabelWidth">
+          <el-input v-model="formPwd.old" style="width: 300px" type="password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop="new" label="新密码" :label-width="formLabelWidth">
+          <el-input v-model="formPwd.new" style="width: 300px" type="password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop="new2" label="确认密码" :label-width="formLabelWidth">
+          <el-input v-model="formPwd.new2" style="width: 300px" type="password" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer" style="text-align: center;">
+        <el-button @click="dialogPwdVisible = false">取 消</el-button>
+        <el-button :loading="pwdLoading" type="primary" @click="pwdUpdateSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-container>
 </template>
 
@@ -60,6 +99,7 @@ export default class Login extends Vue {
   $refs: any
 
   @Action('auth/login') login: any
+  @Action('auth/changePwd') changePwd: any
   
   rules: any = {
     name: [
@@ -86,15 +126,19 @@ export default class Login extends Vue {
         this.login(this.form).then((res) => {
           if (res.code === 200) {
             const { needUpdatePsw } = res.data
-            if (!needUpdatePsw || !Number(needUpdatePsw)) {
+            
+            if (!needUpdatePsw || needUpdatePsw === 0) {
               this.$message.success('登录成功')
-            } else {
-              this.$message.success('密码即将过期')
-            }
 
-            setTimeout(() => {
-              this.$router.replace('/')
-            }, 1000)
+              setTimeout(() => {
+                this.$router.replace('/')
+              }, 1000)
+            }
+            else if (needUpdatePsw === 1) {
+
+              this.dialogDelVisible = true
+              return
+            }
           }
         }).catch(() => {
           this.loading = false
@@ -109,8 +153,102 @@ export default class Login extends Vue {
     }
   }
 
-  mounted() {
+  handle2Login() {
+    this.$router.replace('/')
   }
+
+  dialogDelLoading: boolean = false
+  dialogDelVisible: boolean = false
+  confirmDel() {
+    this.loading = false
+
+  // this.$message.success('密码即将过期')
+  // this.$message.success('密码已过期，请重新修改密码')
+  // setTimeout(() => {
+    this.formPwd = {
+      old: '',
+      new: '',
+      new2: ''
+    }
+    this.dialogDelVisible = false
+    this.handleChangePwd()
+  // }, 1000)
+  }
+
+  // IM PPPPPPPPassword
+  formLabelWidth: any = '120px'
+  dialogPwdVisible: boolean = false
+  handleChangePwd() {
+    this.dialogPwdVisible = true
+  }
+
+  formPwd: any = {
+    old: '',
+    new: '',
+    new2: ''
+  }
+  validatePass(rule, value, callback) {
+    if (!/^.*(?=.{8,20})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/.test(value)) {
+      callback('密码必须包含大小写字母、数字、特殊字符，长度8至20位')
+    } else {
+      if (this.formPwd.new2 !== '') {
+        this.$refs.formPwd.validateField('new2');
+      }
+      callback();
+    }
+  }
+  validatePass2(rule, value, callback) {
+    if (!/^.*(?=.{8,20})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/.test(value)) {
+      callback('密码必须包含大小写字母、数字、特殊字符，长度8至20位')
+    } else if (value !== this.formPwd.new) {
+      callback(new Error('两次输入密码不一致!'));
+    } else {
+      callback();
+    }
+  }
+  rulesPwd: any = {
+    old: [
+      { required: true, message: '原密码必填', trigger: 'blur' }
+    ],
+    new: [
+      { required: true, validator: this.validatePass, trigger: 'blur' }
+    ],
+    new2: [
+      { required: true, validator: this.validatePass2, trigger: 'blur' }
+    ]
+  }
+
+  pwdLoading: boolean = false
+  pwdUpdateSubmit() {
+    this.$refs.formPwd.validate((valid, msg) => {
+      if (valid) {
+        this.pwdLoading = true
+        this.changePwd({
+          oldPassword: this.formPwd.old,
+          newPassword: this.formPwd.new,
+        }).then(() => {
+          this.$message.success('密码修改成功，请重新登录')
+          this.form = {
+            name: '',
+            password: '',
+          }
+          this.$nextTick(() => {
+            this.$refs.form.resetFields()
+            this.$refs.form.clearValidate()
+          })
+          setTimeout(() => {
+            this.dialogPwdVisible = false
+            this.pwdLoading = false
+          }, 1000)
+        }).catch(() => {
+          this.pwdLoading = false
+        })
+      } else {
+        return false;
+      }
+    });
+  }
+  // END Pwd
 }
 </script>
 
